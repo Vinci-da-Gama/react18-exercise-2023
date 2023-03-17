6cdc4d74db547a74e22e35dd15bafc283319c147
Boa Mia <boamia@Boas-MacBook-Pro.local>
2023-03-17 16:10:41 +1100

    r2023: add rtk and sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.scss';
+
+const store = configureStore({
+  reducer: () => 1,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
 
