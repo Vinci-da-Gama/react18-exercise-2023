25b4dd4a9aac40d7e85ee304cfe8d5c0d551ad35
Boa Mia <boamia@Boas-MacBook-Pro.local>
2023-03-28 06:55:05 +1100

    r2023: self npm package

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from 'react';
+
+import { sayNow } from 'gen-npm-dependency';
+
 function App() {
+  useEffect(() => {
+    sayNow();
+    // return () => {};
+  }, []);
   return <div>Hello world</div>;
 }
 
